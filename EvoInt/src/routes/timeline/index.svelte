29024a45ed71990e520df8bc365f95d8ddd941ac
hamidomar000m <74457184+hamidomar000m@users.noreply.github.com>
2022-07-10 13:17:12 +0200

    timeline half done

--- EvoInt/src/routes/timeline/index.svelte
+++ EvoInt/src/routes/timeline/index.svelte
@@ -6,18 +6,127 @@ import Header from '../../components/Header.svelte';
 <Header />
 
 <div class="p-10 flex justify-center">
-	<div class="relative w-4 h-screen bg-gray-900 rounded-3xl" />
-	<div
-		class="absolute top-40 text-black bg-white z-50 rounded-[100%] w-40 h-40 flex justify-center items-center font-mono text-2xl"
-	>
-		07.03.2022
+	<div class=" flex flex-col items-center content-around gap-40 w-4 py-14 bg-gray-900 rounded-3xl" >
+
+    <div
+      class="white bg-white font-mono text-2xl rounded-sm p-4"
+    >
+      Start
+    </div>
+    <div
+      class="flex flex-row justify-center items-center gap-12 ml-[430px] black"
+    >
+      <div
+        class="flex justify-center items-center black bg-white z-50 rounded-[100%] w-40 h-40 font-mono text-2xl"
+      >
+        07.03.2022
+      </div>
+      <div
+        class="flex justify-center items-center bg-white rounded-[5%] w-96 h-32 font-mono text-2xl"
+      >
+        Hello
+      </div>
+    </div>
+
+    <div
+      class="flex flex-row justify-center items-center gap-12 mr-[430px] black bg-red"
+    >
+      <div
+        class="flex justify-center items-center bg-white rounded-[5%] w-96 h-32 font-mono text-2xl"
+      >
+        Hello
+      </div>
+      <div
+        class="flex justify-center items-center black bg-white z-50 rounded-[100%] w-40 h-40 font-mono text-2xl"
+      >
+        07.03.2022
+      </div>
+    </div>
+
+    <div
+      class="flex flex-row justify-center items-center gap-12 ml-[430px] black bg-red"
+    >
+      <div
+        class="flex justify-center items-center black bg-white z-50 rounded-[100%] w-40 h-40 font-mono text-2xl"
+      >
+        07.03.2022
+      </div>
+      <div
+        class="flex justify-center items-center bg-white rounded-[5%] w-96 h-32 font-mono text-2xl"
+      >
+        Hello
+      </div>
+    </div>
+
+    <div
+      class="flex flex-row justify-center items-center gap-12 mr-[430px] black bg-red"
+    >
+      <div
+        class="flex justify-center items-center bg-white rounded-[5%] w-96 h-32 font-mono text-2xl"
+      >
+        Hello
+      </div>
+      <div
+        class="flex justify-center items-center black bg-white z-50 rounded-[100%] w-40 h-40 font-mono text-2xl"
+      >
+        07.03.2022
+      </div>
+    </div>
+
+    <div
+      class="flex flex-row justify-center items-center gap-12 ml-[430px] black bg-red"
+    >
+      <div
+        class="flex justify-center items-center black bg-white z-50 rounded-[100%] w-40 h-40 font-mono text-2xl"
+      >
+        07.03.2022
+      </div>
+      <div
+        class="flex justify-center items-center bg-white rounded-[5%] w-96 h-32 font-mono text-2xl"
+      >
+        Hello
+      </div>
+    </div>
+
+    <div
+      class="flex flex-row justify-center items-center gap-12 mr-[430px] black bg-red"
+    >
+      <div
+        class="flex justify-center items-center bg-white rounded-[5%] w-96 h-32 font-mono text-2xl"
+      >
+        Hello
+      </div>
+      <div
+        class="flex justify-center items-center black bg-white z-50 rounded-[100%] w-40 h-40 font-mono text-2xl"
+      >
+        07.03.2022
+      </div>
+    </div>
+
 	</div>
-	<div
-		class="absolute top-[32rem] text-black bg-white z-50 rounded-[100%] w-40 h-40 flex justify-center items-center font-mono text-2xl"
-	>
-		16.03.2022
-	</div>
-	<div />
 </div>
 
 <Footer />
+
+<!-- 
+<div class="p-10 flex justify-center">
+	<div class=" flex flex-col conten-between w-4 h-screen bg-gray-900 rounded-3xl" />
+    <div
+      class="absolute top-40 text-black bg-white z-50 rounded-[100%] w-40 h-40 flex justify-center items-center font-mono text-2xl"
+    >
+      07.03.2022
+    </div>
+    <div
+      class="absolute top-[32rem] text-black bg-white z-50 rounded-[100%] w-40 h-40 flex justify-center items-center font-mono text-2xl"
+    >
+      16.03.2022
+    </div>
+    <div
+      class="absolute top-[62rem] text-black bg-white z-50 rounded-[100%] w-40 h-40 flex justify-center items-center font-mono text-2xl"
+    >
+      17.03.2022
+    </div>
+	<div />
+</div>
+
+-->
--- EvoInt/tailwind.config.cjs
+++ EvoInt/tailwind.config.cjs
@@ -5,6 +5,9 @@ module.exports = {
       fontFamily: {
         'evoint': ['Alliance No.1']
       },
+      margin: {
+        '430px': '430px',
+      },
     },
   },
   plugins: [],
