69d4b9ea00d7dfb8722f7fb86e6f2f9e8dda00bf
Marten Kaffler <marten.kaffler@bayer.com>
2022-07-20 08:20:33 +0200

    created new branch for NLP

--- EvoInt/NLP/first-tests.py
+++ EvoInt/NLP/first-tests.py
@@ -2,29 +2,35 @@ import glob
 import os
 import nltk
 import fitz
+import datetime
 from nltk import sent_tokenize
 from sentence_transformers import SentenceTransformer
 from torch import embedding
-
+now = datetime.datetime.now()
 nltk.download('punkt')
 
 model = SentenceTransformer('all-mpnet-base-v2')
 
-folder = r"C:\Users\MDKaf\Workspaces\EvoInt-Tracker-v.2\some-papers\*.pdf"
+folder = r"C:\Users\GLJNI\Workspaces\EvoInt-Tracker-v.2\some-papers\*.pdf"
 
 sentences_dict = {}
+embeddings_dict = {}
 
 for file in glob.glob(rf'{folder}'):
     filepath = os.path.normpath(file)
     #print(file_path)
     contents = ""
-    with fitz.open(r"C:\Users\MDKaf\Workspaces\EvoInt-Tracker-v.2\some-papers\ABAMAS_Distance Polymatrix Coordination Games 2021.pdf") as doc:
+    with fitz.open(file) as doc:
         for page in doc:
             contents += page.get_text()
     contents = contents.replace("\n", " ")
     sentence_list = sent_tokenize(contents)
-    #sentences_dict = {os.path.split(filepath)[1] : sentence_list}
-    embedding1 = model.encode(sentence_list, convert_to_tensor=True)
+    sentences_dict = {os.path.split(filepath)[1] : sentence_list}
+    emgeddings_dict = {os.path.split(filepath)[1] : model.encode(sentence_list, convert_to_tensor=True)}
 #print("---", filepath, "---")
 #print(contents)
-print(embedding1)
+now2 = datetime.datetime.now()
+time = now2-now
+
+print(embeddings_dict)
+print(time)
--- EvoInt/NLP/test.py
+++ EvoInt/NLP/test.py
@@ -1,3 +1,21 @@
-string = " test\ntest"
-print(string)
-print(string.replace("\n", " "))
+from sentence_transformers import SentenceTransformer, util
+model = SentenceTransformer('all-mpnet-base-v2')
+
+sentences1 = ['The cat sits outside',
+             'A man is playing guitar',
+             'The new movie is awesome']
+
+sentences2 = ['The kitty is sitting in the garden',
+              'A woman watches TV',
+              'The new movie is so great']
+
+#Compute embedding for both lists
+embeddings1 = model.encode(sentences1, convert_to_tensor=True)
+embeddings2 = model.encode(sentences2, convert_to_tensor=True)
+
+#Compute cosine-similarities
+cosine_scores = util.cos_sim(embeddings1, embeddings2)
+
+#Output the pairs with their score
+for i in range(len(sentences1)):
+    print("{} \t\t {} \t\t Score: {:.4f}".format(sentences1[i], sentences2[i], cosine_scores[i][i]))
